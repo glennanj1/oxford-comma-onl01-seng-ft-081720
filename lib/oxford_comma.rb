e4077feca3edb60c35d3ef774db849824871c115
John Glennan <glennan20@gmail.com>
2020-08-21 18:02:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.join(", , , , and")
+    array[0].join(",")
+    array[1].join(",")
+    array[2].join("and")
   else
     array.join
   end
