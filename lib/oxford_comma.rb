b08f2f1e64f4acd8d2ef2868cfa6296c29716ccb
John Glennan <glennan20@gmail.com>
2020-08-21 17:51:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,7 @@
 def oxford_comma(array)
-  #convert array returns array String
-  array.each do |s|
-    s.split("")
-    array << s
-  end
-  s
+  
+  array = array.split(" ")
+  puts array
     
 
 end
